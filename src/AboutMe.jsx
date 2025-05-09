7d20895c9f680b616fbcbd96c02d1f931d7b2714
unknown <hylataylor@outlook.com>
2025-05-08 17:35:21 -0700

    Fixed numerous cosmetic issues

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -4,19 +4,25 @@ import './AboutMe.css'
 function AboutMe({isMobile}) {
     if(isMobile){
         return(
+                <>
+                <title>About</title>
                 <div className="container">  
                     <h1 className="gradHeading-mobile"><span className="gradText">ABOUT ME</span></h1>
-                    <img src={profile} align="top" id="profile-mobile"/>
+                     <img src={profile} align="top" id="profile-mobile"/>
                     <p className = "description description-mobile">Hello there! I’m <span style={{color: "#f78de7"}}>Hyla Taylor</span> (she/her) - a programmer, storyteller, and occasional artist. I’m currently pursuing a Bachelors of Science in Computer Science at the University of Victoria. When I’m not studying you can find me taking part in Victoria’s flourishing <span style={{color:"#5BCEFA"}}>tra</span><span style={{color:"#F5A9B8"}}>ns </span>com<span style={{color:"#F5A9B8"}}>mun</span><span style={{color:"#5BCEFA"}}>ity</span>, doing volunteer work, or relaxing at home in the beautiful Quw'utsun Valley.</p>
                 </div>
+                </>
         )
     } else {
         return(
-                <div className="container about-container">  
-                    <img src={profile} align="left" id="profile"/>
-                    <h1 className="gradHeading-desktop"><span className="gradText">ABOUT ME</span></h1>
-                    <p className = "description description-desktop">Hello there! I’m <span style={{color: "#f78de7"}}>Hyla Taylor</span> (she/her) - a programmer, storyteller, and occasional artist. I’m currently pursuing a Bachelors of Science in Computer Science at the University of Victoria. When I’m not studying you can find me taking part in Victoria’s flourishing <span style={{color:"#5BCEFA"}}>tra</span><span style={{color:"#F5A9B8"}}>ns </span>com<span style={{color:"#F5A9B8"}}>mun</span><span style={{color:"#5BCEFA"}}>ity</span>, doing volunteer work, or relaxing at home in the beautiful Quw'utsun Valley.</p>
-                </div>
+            <>
+            <title>About</title>
+            <div className="container about-container">  
+                <img src={profile} align="left" id="profile"/>
+                <h1 className="gradHeading-desktop"><span className="gradText">ABOUT ME</span></h1>
+                <p className = "description description-desktop">Hello there! I’m <span style={{color: "#f78de7"}}>Hyla Taylor</span> (she/her) - a programmer, storyteller, and occasional artist. I’m currently pursuing a Bachelors of Science in Computer Science at the University of Victoria. When I’m not studying you can find me taking part in Victoria’s flourishing <span style={{color:"#5BCEFA"}}>tra</span><span style={{color:"#F5A9B8"}}>ns </span>com<span style={{color:"#F5A9B8"}}>mun</span><span style={{color:"#5BCEFA"}}>ity</span>, doing volunteer work, or relaxing at home in the beautiful Quw'utsun Valley.</p>
+            </div>
+            </>
         )
     } 
 }
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,17 +2,23 @@
 function Projects({isMobile}) {
     if(isMobile){
         return(
+            <>
+            <title>Projects</title>
             <div className="container">  
                 <h1 className="gradHeading-mobile"><span className="gradText">COMING SOON</span></h1>
                 <p class="description description-mobile">there's nothing here right now, but maybe someday!</p>
             </div>
+            </>
         )
     } else {
         return(
-                <div className="container projects-container">  
-                    <h1 className="gradHeading-desktop"><span className="gradText">COMING SOON</span></h1>
-                    <p class="description description-desktop">there's nothing here right now, but maybe someday!</p>
-                </div>
+            <>
+            <title>Projects</title>
+            <div className="container projects-container">  
+            <h1 className="gradHeading-desktop"><span className="gradText">COMING SOON</span></h1>
+                <p class="description description-desktop">there's nothing here right now, but maybe someday!</p>
+            </div>
+            </>
         )
     }
     
